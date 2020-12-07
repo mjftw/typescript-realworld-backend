53afb72b0486ea61a899f3e40f3446022e8954be
Merlin Webster <mjftwebster@gmail.com>
2020-12-07 09:49:21 +0000

    Added database tables creation SQL

--- src/migrations/dbinit.sql
+++ src/migrations/dbinit.sql
@@ -1,11 +1,91 @@
 CREATE TABLE users (
-    id bigserial NOT NULL PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    age int
+    user_id bigint GENERATED ALWAYS AS IDENTITY,
+    email varchar(200) NOT NULL,
+    username varchar(50) NOT NULL,
+    bio text,
+    image text,
+    PRIMARY KEY(user_id)
 );
 
-INSERT INTO users (name, age)
-VALUES
-    ('Merlin', 27),
-    ('John', 62),
-    ('Marci', null);
+CREATE TABLE articles (
+    article_id bigint GENERATED ALWAYS AS IDENTITY,
+    slug varchar(100) NOT NULL,
+    title varchar(200) NOT NULL,
+    description varchar(500),
+    body text NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    author_id bigint NOT NULL,
+    PRIMARY KEY(article_id),
+    FOREIGN KEY(author_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+    comment_id bigint GENERATED ALWAYS AS IDENTITY,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    body text NOT NULL,
+    author_id bigint NOT NULL,
+    PRIMARY KEY(comment_id),
+    FOREIGN KEY(author_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE tags (
+    tag_id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(50) NOT NULL,
+    PRIMARY KEY(tag_id)
+);
+
+CREATE TABLE users_followed_users (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    follower_user_id bigint NOT NULL,
+    followed_user_id bigint NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(follower_user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY(followed_user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE users_favorited_articles (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    user_id bigserial NOT NULL,
+    article_id bigserial NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY(article_id)
+        REFERENCES articles(article_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE articles_tags (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    article_id bigint NOT NULL,
+    tag_id int NOT NULL,
+    FOREIGN KEY(article_id)
+        REFERENCES articles(article_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY(tag_id)
+        REFERENCES tags(tag_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE articles_comments (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    article_id bigint NOT NULL,
+    comment_id bigint NOT NULL,
+    FOREIGN KEY(article_id)
+        REFERENCES articles(article_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY(comment_id)
+        REFERENCES comments(comment_id)
+        ON DELETE CASCADE
+);
